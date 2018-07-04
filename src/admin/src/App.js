650193510cf6e057b030a12632a187e2ff9b3993
Tim Swiggers <tim.swiggers@hotmail.com>
2018-07-04 15:51:30 +0200

    connected the admin to server!

--- src/admin/src/App.js
+++ src/admin/src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import * as signalR from '@aspnet/signalr';
+
+const server = 'http://localhost:60860';
 
 class App extends Component {
+
+  connection;
+
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      connected: false
+    }
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl(`${server}/hub/client`)
+      .build();
+
+    await this.connection.start();
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    this.setState({ loading: false, connected: true });
+  }
   render() {
     return (
-      <h1>
-        This is the admin
-      </h1>
+      <div>
+        <h1>
+          This is the admin
+        </h1>
+        {this.state.loading && <em>loading...</em>}
+        {this.state.connected && <strong>Connected!</strong>}
+      </div>
     );
   }
 }
